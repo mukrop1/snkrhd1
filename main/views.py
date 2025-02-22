494e8c382f5e1d683eec571a167f8a951ea67c9c
Sergey Kropachev <kropachev_1998@mail.ru>
2025-02-22 13:42:19 +0500

    html and home page rdy

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context = {"title": "SNKRHDS - Главная", "content": "SNKRHDS"}
+    return render(request, "main/index.html", context)
+
+
+def about(request):
+    context = {
+        "title": "SNKRHDS - О нас",
+        "content": "SNKRHDS",
+        "text_on_page": "туттутутутутуту",
+    }
+    return render(request, "main/about.html", context)
