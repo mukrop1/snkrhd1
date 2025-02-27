9368f143525c47f0756510bdec04288fef3ed861
Sergey Kropachev <kropachev_1998@mail.ru>
2025-02-27 14:04:54 +0500

    paginats.GET parms.

--- goods/views.py
+++ goods/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import get_list_or_404, render
 from .models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+    
+    if category_slug == "all":
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
-    goods = Products.objects.all()
-
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(int(page))
+    
     context = {
         "title": " Home - Каталог",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
