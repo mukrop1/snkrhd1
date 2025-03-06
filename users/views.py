41c544fab08a17dd3aa3a829f076a2418b5471c8
Sergey Kropachev <kropachev_1998@mail.ru>
2025-03-06 13:11:07 +0500

    cart's views+fixed  templates+some fixes

--- users/views.py
+++ users/views.py
@@ -18,8 +18,9 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f"{username}, вход выполнен")
 
-                if request.POST.get("next", None):
-                    return HttpResponseRedirect(request.POST.get("next"))
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
 
                 return HttpResponseRedirect(reverse("main:index"))
     else:
