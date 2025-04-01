b781bdadf5c33f99c81fc400f25122d2ec97a8f2
Sergey Kropachev <kropachev_1998@mail.ru>
2025-04-01 16:50:25 +0500

    creating an order is rdy

--- snkrhd/urls.py
+++ snkrhd/urls.py
@@ -30,6 +30,7 @@ urlpatterns = (
         path("catalog/", include("goods.urls", namespace="catalog")),
         path("user/", include("users.urls", namespace="user")),
         path("cart/", include("carts.urls", namespace="cart")),
+        path("orders/", include("orders.urls", namespace="orders")),
     ]
     + debug_toolbar_urls()
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from orders.admin import OrderTabulareAdmin
 from carts.admin import CartTabAdmin
 from .models import User
 
@@ -9,4 +9,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "first_name", "last_name", "email"]
     search_fields = ["username", "first_name", "last_name", "email"]
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
