b004b932422c15a6c394fb4ef98387d6c0cd2a8a
Sergey Kropachev <kropachev_1998@mail.ru>
2025-04-02 13:09:29 +0500

    cbv views for goods&main

--- main/views.py
+++ main/views.py
@@ -1,23 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 from goods.models import Categories
 
 
-def index(request):
+class IndexView(TemplateView):
+    template_name = "main/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "SNKRHDS - Главная"
+        context["content"] = "SNKRHDS"
+        return context
 
 
-    context = {
-        "title": "SNKRHDS - Главная",
-        "content": "SNKRHDS",
-    }
-    return render(request, "main/index.html", context)
+class AboutView(TemplateView):
+    template_name = "main/about.html"
 
-
-def about(request):
-    context = {
-        "title": "SNKRHDS - О нас",
-        "content": "SNKRHDS",
-        "text_on_page": "туттутутутутуту",
-    }
-    return render(request, "main/about.html", context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "SNKRHDS - О нас"
+        context["content"] = "SNKRHDS"
+        context["text_on_page"] = "туттутутутутуту"
+        return context
