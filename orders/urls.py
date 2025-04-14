b2e257793329f7cb51bdf7ce8bdb50e2b5a15bb9
Sergey Kropachev <kropachev_1998@mail.ru>
2025-04-14 18:36:13 +0500

    cbv rdy.

--- orders/urls.py
+++ orders/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-
 from orders import views
 
 app_name = "orders"
 
 urlpatterns = [
-    path("create-order/", views.create_order, name="create_order"),
+    path("create-order/", views.CreateOrderView.as_view(), name="create_order"),
 ]
