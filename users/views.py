b07ef0c1a4d7e4be6693c06e2cb3b5291d432c9f
Sergey Kropachev <kropachev_1998@mail.ru>
2025-04-15 21:19:28 +0500

    cac

--- users/views.py
+++ users/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, TemplateView, UpdateView
 from carts.models import Cart
+from common.mixins import CacheMixin
 from orders.models import Order, OrderItem
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
@@ -77,7 +78,7 @@ class UserRegistrationView(CreateView):
         return context
 
 
-class UserProfileView(LoginRequiredMixin, UpdateView):
+class UserProfileView(LoginRequiredMixin, CacheMixin, UpdateView):
     template_name = "users/profile.html"
     form_class = ProfileForm
     success_url = reverse_lazy("users:profile")
@@ -96,7 +97,7 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = "Home - Кабинет"
-        context["orders"] = (
+        orders = (
             Order.objects.filter(user=self.request.user)
             .prefetch_related(
                 Prefetch(
@@ -105,6 +106,10 @@ class UserProfileView(LoginRequiredMixin, UpdateView):
                 )
             )
             .order_by("-id")
+        )
+
+        context["orders"] = self.set_get_cache(
+            orders, f"user_{self.request.user.id}_orders", 60
         )
         return context
 
